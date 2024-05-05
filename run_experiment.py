14e08b922408b12349218d6f86f936acd4759d36
Tim Williamson <tim.williamson@kaluza.com>
2024-05-05 16:01:50 +0100

    split curriculum learning experiment in to 2 parts

--- run_experiment.py
+++ run_experiment.py
@@ -1,7 +1,10 @@
 import sys
 
 from rl_runners.parallel_run_curriculum_learning_experiment_1 import run_curriculum_learn_experiment
+from rl_runners.parallel_run_curriculum_learning_experiment_2 import run_curriculum_learn_experiment_part_2
 
 if __name__ == "__main__":
     if sys.argv[2] == "curriculum_experiment":
         run_curriculum_learn_experiment(sys.argv[1])
+    elif sys.argv[2] == "curriculum_experiment_2":
+        run_curriculum_learn_experiment_part_2(sys.argv[1])
