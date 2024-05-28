1c8f8ea69dd2354a8bdb299923502f838798beb9
Tim Williamson <tim.williamson@kaluza.com>
2024-05-28 21:32:37 +0100

    set bind_core false

--- rl_runners/series_run_curriculum_learning_experiment.py
+++ rl_runners/series_run_curriculum_learning_experiment.py
@@ -5,6 +5,6 @@ def run_curriculum_learn_experiment(savepath):
     difficulties = [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 
     for difficulty in difficulties:
-       run_hac(f"{savepath}/non-curriculum-{difficulty}", 100, difficulty, False, num_cpu=8, bind_core=1)
+       run_hac(f"{savepath}/non-curriculum-{difficulty}", 100, difficulty, False, num_cpu=10, bind_core=0)
 
-    run_hac(f"{savepath}/curriculum-{difficulty}", 100, 0.0, True, num_cpu=8, bind_core=1)
+    run_hac(f"{savepath}/curriculum-{difficulty}", 100, 0.0, True, num_cpu=10, bind_core=0)
