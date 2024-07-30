bbf679a0bc02c7a98853291938b9599281ce08cc
Tim Williamson <tim.williamson@kaluza.com>
2024-07-30 20:38:19 +0100

    add heirarchical learning experiment

---
+++ rl_runners/run_heirarchical_learning_experiment_with_env_in_state.py
@@ -0,0 +1,7 @@
+from rl_runners.rl_baselines_hac_mujoco import run_hac
+
+def run_heirarchical_learning_experiment_with_env_in_state(savepath):
+    level_nums = [1,2,3,4,5]
+
+    for num_levels in level_nums:
+        run_hac(f"{savepath}/non-hierachies-{num_levels}", 100, 0.0, True, num_cpu=10, nn_size=256)
