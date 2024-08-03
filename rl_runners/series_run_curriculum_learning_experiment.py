c2b11ffc450c123bcb8e2699936d34956e386060
Tim Williamson <tim.williamson@kaluza.com>
2024-08-03 11:37:24 +0100

    put back in lower difficulties

--- rl_runners/series_run_curriculum_learning_experiment.py
+++ rl_runners/series_run_curriculum_learning_experiment.py
@@ -2,7 +2,7 @@ from rl_runners.rl_baselines_hac_mujoco import run_hac
 
 def run_curriculum_learn_experiment(savepath):
     # without curriculum learning:
-    difficulties = [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+    difficulties = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 
     for difficulty in difficulties:
        run_hac(f"{savepath}/non-curriculum-{difficulty}", 50, difficulty, False, num_cpu=10, nn_size = 256)
