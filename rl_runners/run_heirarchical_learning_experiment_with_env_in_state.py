4216c7d8a20d2c7fa42ac7e8d224cffc42c75031
Tim Williamson <tim.williamson@kaluza.com>
2024-08-03 11:30:22 +0100

    update experiments

--- rl_runners/run_heirarchical_learning_experiment_with_env_in_state.py
+++ rl_runners/run_heirarchical_learning_experiment_with_env_in_state.py
@@ -4,4 +4,4 @@ def run_heirarchical_learning_experiment_with_env_in_state(savepath):
     level_nums = [1,2,3,4,5]
 
     for num_levels in level_nums:
-        run_hac(f"{savepath}/non-hierachies-{num_levels}", 100, 0.0, True, num_cpu=10, nn_size=256)
+        run_hac(f"{savepath}/non-hierachies-{num_levels}", 50, 0.0, increasing_difficulty = True, num_cpu=10, nn_size=256)
