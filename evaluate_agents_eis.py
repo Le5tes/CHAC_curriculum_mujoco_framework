6a2be54df297ef6d5f5e894b62d3a748aee32539
Tim Williamson <tim.williamson@kaluza.com>
2024-08-26 09:03:31 +0100

    add evaluate with eis

--- evaluate_agents_eis.py
+++ evaluate_agents_eis.py
@@ -5,5 +5,5 @@ from test_runners.assess_trained_model import run_assess_hac_ant_mujoco
 if __name__ == "__main__":
    path = sys.argv[1]
    for arg in sys.argv[2:]:
-      run_assess_hac_ant_mujoco(path + arg, path + arg + ".results.json", include_env_in_state=False)
+      run_assess_hac_ant_mujoco(path + arg, path + arg + ".results.json", include_env_in_state=True)
    
